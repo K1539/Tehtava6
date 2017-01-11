f249f085eae66e73ba0260481ebb7dbfdd421091
K1539 <K1539@ICT-421-WS-29>
2017-01-11 12:01:28 +0200

    Valmis

--- Tehtava6/Program.cs
+++ Tehtava6/Program.cs
@@ -10,7 +10,14 @@ namespace Tehtava6
     {
         static void Main(string[] args)
         {
+            double KulutusKm = 0.0702;
+            double BensanHintaLtr = 1.595;
+           
+            Console.Write("Anna matka > ");
+            int KuljettuMatka = int.Parse(Console.ReadLine());
+            double BensanKulutus = (KulutusKm * KuljettuMatka);
 
+            Console.WriteLine("Bensaa kuluu " + BensanKulutus + " litraa, kustannus " + (BensanKulutus * BensanHintaLtr) + " euroa");
         }
     }
 }
